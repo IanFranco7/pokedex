8dc0c6870839c21df35927a7cca2d604d974e4ff
Ian França <93686060+IanFranco7@users.noreply.github.com>
2023-01-11 20:03:15 -0300

    Update README

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/***************Variaveis***************/
 const pokemonName = document.querySelector('.pokemon_name')
 const pokemonNumber = document.querySelector('.pokemon_number')
 const pokemonImage = document.querySelector('.pokemon_image')
@@ -9,8 +10,8 @@ const btnPrev = document.querySelector('.btn-prev')
 const btnNext = document.querySelector('.btn-next')
 
 let searchPokemon = 1
-/* você pegou o formulário, pq vc tem que ouvir o evento de submit tlgd?
-não teria como capturar oe vento se pegasse direto no input, se pá ate teria mas seria mais complicado então porra, p facilitar já pega o formulario logo q tem o submit tlgd p ecutar o evento pego ua vz*/
+
+/***************Funções***************/
 
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`) 
@@ -18,13 +19,7 @@ const fetchPokemon = async (pokemon) => {
         const data = await APIResponse.json();
         return data;
     }
-    // depois de pegar a resposta da api temos que traduzir os dados pra json
-    
-    
 }   
-
-// AHHH agr eu peguei a vz, essa render pokemon ela é uma função que recebe a constante data vindo da outra função que é fetch, essa fetch, ela vai receber um parametro p pegar os dados do pokemon, e vai transformar em json. e vai retornar esse json ai. Então, a render pokemon é a função "pai", pq ela vai receber um parametro pokemon, nisso esse a fetchpokemon vai receber o MESMO parametro que a render tá recebendo, já que a fetch está dentro da render, e então agora a render com os dados do pokemon em JSON, so vai extrair e por dentro do html. porra muito brabo voltando at rabalhar com função e logica ai sim clhh boaaa!
-
 
 const renderPokemon = async (pokemon) => {
     pokemonName.innerHTML = 'Carregando...'
@@ -44,6 +39,7 @@ const renderPokemon = async (pokemon) => {
         pokemonNumber.innerHTML = ''
     }
 }
+/***************Eventos***************/
 
 form.addEventListener('submit', (event)=>{
     event.preventDefault();
@@ -55,7 +51,6 @@ btnPrev.addEventListener('click', ()=>{
         searchPokemon-= 1
         renderPokemon(searchPokemon)
     }
-    
 })
 
 btnNext.addEventListener('click', ()=>{
